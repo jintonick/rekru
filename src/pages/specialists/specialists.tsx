e0ed5dcf7777bdef5571caccb98744ad5c906cbb
jintonick <viktor.krasuyk@gmail.com>
2024-06-15 00:24:52 +0300

    some new style

--- src/pages/specialists/specialists.tsx
+++ src/pages/specialists/specialists.tsx
@@ -1,6 +1,6 @@
 // ./src/ResumeListing.tsx
 import React, { useState } from 'react';
-import { Pagination } from 'antd';
+import {Button, Checkbox, Pagination, Radio, Select} from 'antd';
 import { resumes } from './data2';
 import search_gray from '../../imgs/search_gray.svg'
 import share from '../../imgs/share.svg'
@@ -32,11 +32,11 @@ const ResumeListing: React.FC = () => {
                             </button>
                         </div>
                         <ul>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="region1" name="region1" /><label htmlFor="region1">Москва и Московская область</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="region2" name="region2" /><label htmlFor="region2">Санкт-Петербург</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="region3" name="region3" /><label htmlFor="region3">Казань</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="region4" name="region4" /><label htmlFor="region4">Владимир</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px]" type="checkbox" id="region5" name="region5" /><label htmlFor="region5">Астрахань</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px]" type="checkbox" id="region1" name="region1" /><label htmlFor="region1">Москва и Московская область</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px] " type="checkbox" id="region2" name="region2" /><label htmlFor="region2">Санкт-Петербург</label></li>
+                            <li className="flex items-center"><Checkbox className=" border-[#DBDBDB] mr-[8px] " type="checkbox" id="region3" name="region3" /><label htmlFor="region3">Казань</label></li>
+                            <li className="flex items-center"><Checkbox className=" border-[#DBDBDB] mr-[8px] " type="checkbox" id="region4" name="region4" /><label htmlFor="region4">Владимир</label></li>
+                            <li className="flex items-center"><Checkbox className=" border-[#DBDBDB] mr-[8px]" type="checkbox" id="region5" name="region5" /><label htmlFor="region5">Астрахань</label></li>
                         </ul>
                     </div>
                     <div className="mb-[20px]">
@@ -48,46 +48,46 @@ const ResumeListing: React.FC = () => {
                             </button>
                         </div>
                         <ul>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="speciality1" name="speciality1" /><label htmlFor="speciality1">Менеджер</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="speciality2" name="speciality2" /><label htmlFor="speciality2">Курьер</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="speciality3" name="speciality3" /><label htmlFor="speciality3">Инженер</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px] mb-[8px]" type="checkbox" id="speciality4" name="speciality4" /><label htmlFor="speciality4">Повар</label></li>
-                            <li className="flex items-center"><input className="h-[24px] w-[24px] border-[#DBDBDB] mr-[8px]" type="checkbox" id="speciality5" name="speciality5" /><label htmlFor="speciality5">Архитектор</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px]" type="checkbox" id="speciality1" name="speciality1" /><label htmlFor="speciality1">Менеджер</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px]" type="checkbox" id="speciality2" name="speciality2" /><label htmlFor="speciality2">Курьер</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px]" type="checkbox" id="speciality3" name="speciality3" /><label htmlFor="speciality3">Инженер</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px]" type="checkbox" id="speciality4" name="speciality4" /><label htmlFor="speciality4">Повар</label></li>
+                            <li className="flex items-center"><Checkbox className="border-[#DBDBDB] mr-[8px]" type="checkbox" id="speciality5" name="speciality5" /><label htmlFor="speciality5">Архитектор</label></li>
                         </ul>
                     </div>
                     <div className="mb-[20px]">
                         <h3 className="font-bold mb-[14px]">Пол</h3>
                         <ul>
-                            <li className="flex items-center "><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="male" name="gender" /><label htmlFor="male">Мужской</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="female" name="gender" /><label htmlFor="female">Женский</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="any" name="gender" /><label htmlFor="any">Неважно</label></li>
+                            <li className="flex items-center "><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="male" name="gender" /><label htmlFor="male">Мужской</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="female" name="gender" /><label htmlFor="female">Женский</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="any" name="gender" /><label htmlFor="any">Неважно</label></li>
                         </ul>
                     </div>
                     <div className="mb-[20px]">
                         <h3 className="font-bold mb-2">Опыт работы</h3>
                         <ul>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience1" name="experience" /><label htmlFor="experience1">Неважно</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience2" name="experience" /><label htmlFor="experience2">От 1 года до 3 лет</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience3" name="experience" /><label htmlFor="experience3">От 3 лет до 5 лет</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience4" name="experience" /><label htmlFor="experience4">Больше 5 лет</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience1" name="experience" /><label htmlFor="experience1">Неважно</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience2" name="experience" /><label htmlFor="experience2">От 1 года до 3 лет</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience3" name="experience" /><label htmlFor="experience3">От 3 лет до 5 лет</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="experience4" name="experience" /><label htmlFor="experience4">Больше 5 лет</label></li>
                         </ul>
                     </div>
                     <div className="mb-[20px]">
                         <h3 className="font-bold mb-2">Готовность к переезду</h3>
                         <ul>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="relocation1" name="relocation" /><label htmlFor="relocation1">Не готов</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="relocation2" name="relocation" /><label htmlFor="relocation2">Готов внутри страны</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="relocation3" name="relocation" /><label htmlFor="relocation3">Готов по всему миру</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="relocation1" name="relocation" /><label htmlFor="relocation1">Не готов</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="relocation2" name="relocation" /><label htmlFor="relocation2">Готов внутри страны</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="relocation3" name="relocation" /><label htmlFor="relocation3">Готов по всему миру</label></li>
                         </ul>
                     </div>
                     <div className="mb-[20px]">
                         <h3 className="font-bold mb-2">Образование</h3>
                         <ul>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education1" name="education" /><label htmlFor="education1">Больше одного высшего</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education2" name="education" /><label htmlFor="education2">Неоконченное высшее</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education3" name="education" /><label htmlFor="education3">Среднее специальное</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education4" name="education" /><label htmlFor="education4">Среднее</label></li>
-                            <li className="flex items-center"><input className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education5" name="education" /><label htmlFor="education5">Неважно</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education1" name="education" /><label htmlFor="education1">Больше одного высшего</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education2" name="education" /><label htmlFor="education2">Неоконченное высшее</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education3" name="education" /><label htmlFor="education3">Среднее специальное</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education4" name="education" /><label htmlFor="education4">Среднее</label></li>
+                            <li className="flex items-center"><Radio className="h-[16px] w-[16px] mr-[10px]" type="radio" id="education5" name="education" /><label htmlFor="education5">Неважно</label></li>
                         </ul>
                     </div>
                 </div>
@@ -109,16 +109,26 @@ const ResumeListing: React.FC = () => {
                         </div>
                     </header>
                     <div className="flex justify-start text-[#777777] gap-[30px] my-[10px] px-[30px]">
-                        <select className="w-[131px]">
-                            <option>За всё время</option>
-                            <option>Последние 24 часа</option>
-                            <option>Последние 7 дней</option>
-                            <option>Последний месяц</option>
-                        </select>
-                        <select className="w-[126px]">
-                            <option>Популярные</option>
-                            <option>Недавние</option>
-                        </select>
+                        <Select
+                            defaultValue="1"
+                            style={{ width: 170 }}
+                            className="border-none"
+                            options={[
+                                { value: '1', label: 'За всё время' },
+                                { value: '2', label: 'Последние 24 часа' },
+                                { value: '3', label: 'Последние 7 дней' },
+                                { value: '4', label: 'Последний месяц' },
+                            ]}
+                        />
+                        <Select
+                            defaultValue="1"
+                            style={{ width: 140 }}
+                            className="border-none"
+                            options={[
+                                { value: '1', label: 'Популярные' },
+                                { value: '2', label: 'Недавние' },
+                            ]}
+                        />
                     </div>
                     <div className="h-screen flex flex-col justify-between">
                         <div>
@@ -163,12 +173,13 @@ const ResumeListing: React.FC = () => {
                                                 <button className="w-[118px] h-[44px] text-white bg-[#2A5AB8] rounded-[7px]">Написать</button>
                                                 <button className="w-[95px] h-[44px] bg-[#DBDBDB] rounded-[7px]">Сохранить</button>
                                             </div>
-                                            <button
+                                            <Button
+                                                type="link"
                                                 onClick={() => toggleExpand(index)}
-                                                className="mt-4 text-[#2A5AB8]"
+                                                className="mt-4 text-[#2A5AB8] text-[16px]"
                                             >
                                                 {expandedResume === index ? 'Свернуть' : 'Развернуть'}
-                                            </button>
+                                            </Button>
                                         </div>
                                     </div>
                                 </div>
